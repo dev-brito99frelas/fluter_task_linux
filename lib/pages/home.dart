8b94431c0b50439049f4bc9125a019265728e942
Okiagari-Koboshi <clodoaldo.neto.116@ufrn.edu.br>
2022-07-27 17:04:47 -0300

    modified Appbar color and placed Drawer in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.yellow[700],
         title: const Text('Home'),
       ),
+      drawer: const Drawer(),
       body: const Center(
         child: Text('Home'),
       ),
