338d31993e8dde5b91375e724d457057acc1b78c
Okiagari-Koboshi <clodoaldo.neto.116@ufrn.edu.br>
2022-07-27 15:34:06 -0300

    added textfield for email,password and controllers

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: const Center(
-        child: Text('Login'),
+      body: Column(
+        children: <Widget>[
+          const Text("Bem vindo a pagina de login"),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'E-mail',
+                hintText: "exemplo@gmail.com",
+                errorText: null,
+              ),
+              onChanged: onchangedContext,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: TextField(
+              controller: senhaController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                hintText: "Exam#Ple@8376%",
+                errorText: null,
+              ),
+              obscureText: true,
+              onChanged: onchangedContextPassword,
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  void onchangedContext(String dado) {}
+
+  void onchangedContextPassword(String dado) {}
 }
