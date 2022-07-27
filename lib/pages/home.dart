16b034f6de79ff01ff0969a8001c0447101252bc
Okiagari-Koboshi <clodoaldo.neto.116@ufrn.edu.br>
2022-07-27 17:16:34 -0300

    change home state, and starting listview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> tarefas = [];
+  final TextEditingController tarefaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +19,19 @@ class Home extends StatelessWidget {
         title: const Text('Home'),
       ),
       drawer: const Drawer(),
-      body: const Center(
-        child: Text('Home'),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          ListTile(
+            title: const Text('Tarefa '),
+            subtitle: const Text('Tarefa: 20/11/2020'),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {},
+            ),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
