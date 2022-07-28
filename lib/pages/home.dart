e1a53550090ee07ac6b6d94108565bc5125a42a2
Okiagari-Koboshi <clodoaldo.neto.116@ufrn.edu.br>
2022-07-27 22:33:09 -0300

    add list items and solved infinite row width problem

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> tarefas = [];
+  List<String> tasks = [
+    "example 1",
+    "example 2",
+    "example 3",
+    "example 4",
+    "example 5"
+  ];
   final TextEditingController tarefaController = TextEditingController();
+  late String iterator;
 
   @override
   Widget build(BuildContext context) {
@@ -19,20 +26,63 @@ class _HomeState extends State<Home> {
         title: const Text('Home'),
       ),
       drawer: const Drawer(),
-      body: ListView(
-        shrinkWrap: true,
+      body: Column(
         children: [
-          ListTile(
-            title: const Text('Tarefa '),
-            subtitle: const Text('Tarefa: 20/11/2020'),
-            trailing: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () {},
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: TextField(
+                    controller: tarefaController,
+                    decoration: const InputDecoration(
+                      labelText: 'Adicione uma tarefa',
+                      hintText: "...@estudar typeorm",
+                      errorText: null,
+                    ),
+                    onChanged: onchangedTarefa,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: onPressedAdd,
+                    child: const Text("+"),
+                  ),
+                )
+              ],
             ),
-            onTap: () {},
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              for (iterator in tasks)
+                ListTile(
+                  leading: const Icon(Icons.assignment, size: 24.0),
+                  title: Text('Tarefa $iterator'),
+                  subtitle: const Text('Tarefa: 20/11/2020'),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("delete $iterator");
+                    },
+                  ),
+                  onTap: () {
+                    // ignore: avoid_print
+                    print(":$iterator");
+                  },
+                ),
+            ],
           ),
         ],
       ),
     );
+  }
+
+  void onchangedTarefa(String algomudou) {}
+  void onPressedAdd() {
+    tarefaController.clear();
   }
 }
